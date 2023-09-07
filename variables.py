47eea6161f78c1e08f379049b106944d70d14e5f
Carl Kevin Galvan <99018607+ckevingalvan@users.noreply.github.com>
2023-09-07 22:09:00 +0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,27 +1,35 @@
-######### Variables ########
-# integer value
-x = 4
-y = 5
-age=19
-# String value
-name = "Abel"
-father_name = "Gideon"
-# Boolean value
-is_student = True
+# ######### Variables ########
+# # integer value
+# x = 4
+# y = 5
+# age=19
+# # String value
+# name = "Abel"
+# father_name = "Gideon"
+# # Boolean value
+# is_student = True
 
-# Printing the values
-print("x: ", x)
-print("y: ", y)
-print(x + y)
+# # Printing the values
+# print("x: ", x)
+# print("y: ", y)
+# print(x + y)
 
-print("name: "+name)
-print(name + " " + father_name)
+# print("name: "+name)
+# print(name + " " + father_name)
 
-print("is_student: ", is_student)
+# print("is_student: ", is_student)
 
-# Combining strings and numbers
-print(name + " " + father_name + " " + str(age) + " years old")
+# # Combining strings and numbers
+# print(name + " " + father_name + " " + str(age) + " years old")
 
 ######### Taking Input ########
+# gname = input("Enter your name: ")
+# print("Hello "+ gname)
+
+# Ask user for age and tell them their in five years
+age = input("Enter your age: ")
+age_in_five_years = int(age) + 5
+print("You will be " + str(age_in_five_years) + " years old in five years.")
+
 
 
