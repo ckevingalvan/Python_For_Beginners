88902cdb5ec6f667f76e1d8b34582ae1fa3b2f80
Carl Kevin Galvan <99018607+ckevingalvan@users.noreply.github.com>
2023-09-07 09:56:12 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,27 @@
+######### Variables ########
+# integer value
+x = 4
+y = 5
+age=19
+# String value
+name = "Abel"
+father_name = "Gideon"
+# Boolean value
+is_student = True
+
+# Printing the values
+print("x: ", x)
+print("y: ", y)
+print(x + y)
+
+print("name: "+name)
+print(name + " " + father_name)
+
+print("is_student: ", is_student)
+
+# Combining strings and numbers
+print(name + " " + father_name + " " + str(age) + " years old")
+
+######### Taking Input ########
+
+
