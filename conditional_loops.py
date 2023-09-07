9e82855a2b846ddbb056cccae48687a66cbb040c
Carl Kevin Galvan <99018607+ckevingalvan@users.noreply.github.com>
2023-09-07 22:25:42 +0800

    Update conditional_loops.py

--- conditional_loops.py
+++ conditional_loops.py
@@ -1,8 +1,16 @@
 ######### Conditional Loops #############
-age = 19
+# age = 19
 
-if age >20:
-    print("You are older than 20.")
+# if age >20:
+#     print("You are older than 20.")
     
-else:
-    print("You are younger than 20")
+# else:
+#     print("You are younger than 20")
+
+# While loop
+num = 0
+
+while num < 10:
+    print(num)
+    num = num + 1
+# -> This will result to infinite printing
