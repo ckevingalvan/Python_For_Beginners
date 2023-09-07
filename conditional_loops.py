285f02fb4b3325ad3caad8f1b1301329e4dec945
Carl Kevin Galvan <99018607+ckevingalvan@users.noreply.github.com>
2023-09-07 22:30:57 +0800

    Update conditional_loops.py

--- conditional_loops.py
+++ conditional_loops.py
@@ -8,9 +8,26 @@
 #     print("You are younger than 20")
 
 # While loop
-num = 0
+# num = 0
 
-while num < 10:
-    print(num)
-    num = num + 1
+# while num < 10:
+#     print(num)
+#     num = num + 1
 # -> This will result to infinite printing
+
+# For loops
+# for num in range(10):
+#     print(num)
+
+# Guessing game
+secret_number = 4
+
+guess = int(input("Enter your guess: "))
+
+while guess != secret_number:
+    if guess < secret_number:
+        print("Guess too low")
+    else:
+        print("Guess too high")
+        
+print("Congratulations you have won!")
