44ce96ba06deacecc1b98ef0a5bc0d70842848c6
Carl Kevin Galvan <99018607+ckevingalvan@users.noreply.github.com>
2023-09-12 16:31:58 +0800

    Update files.py with append

--- files.py
+++ files.py
@@ -1,7 +1,7 @@
 ############## File Handling ############
-file = open("Hello.txt","w")
+file = open("Hello.txt","a")
 # a - append | r - read | w - write
-file.write("Hello CK")
+file.write("\nHello CJ")
 # We need to close the file 
 file.close()
 
