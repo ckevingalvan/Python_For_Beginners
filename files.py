369eb479397cdd041751ef53b907c9337479fdc8
Carl Kevin Galvan <99018607+ckevingalvan@users.noreply.github.com>
2023-09-12 16:47:46 +0800

    Update files.py

--- files.py
+++ files.py
@@ -1,6 +1,37 @@
 ############## File Handling ############
-file = open("Hello.txt","r")
+# Write inside the file
+file = open("Hello.txt","w")
 # a - append | r - read | w - write
-print(file.read())
+file.write("Hello CK")
 # We need to close the file 
 file.close()
+
+# Append information inside the file.
+file = open("Hello.txt","a")
+file.write("\nCK")
+file.close()
+
+# Read content inside the file.
+file = open("Hello.txt","r")
+print(file.read())
+file.close()
+
+# Store every line in a container within the file content
+file = open("Hello.txt","r")
+numbers = file.readlines()
+for items in numbers:
+    print(items.strip())
+file.close()
+
+# Store every line in a container within the file content, then add every numbers
+file = open("Hello.txt","r")
+numbers = file.readlines()
+sum = 0
+for items in numbers:
+    sum += int(items.strip())
+file.close()
+print(sum)
+
+
+
+
